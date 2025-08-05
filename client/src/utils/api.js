558281f2a71882f84b8a06b0c973aba9f951aed3
rutujawable <rutujawable0@gmail.com>
2025-08-05 12:59:14 +0530

    env added

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({ baseURL: process.env.SERVERURL || "https://rating-app-qpj6.onrender.com" });
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem('token');
